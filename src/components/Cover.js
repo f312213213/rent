5bfc3079991b729774d611def179a64516904883
f312213213 <f312213213david@gmail.com>
2021-08-04 20:02:42 +0800

    Delete useless hover effect.

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -8,7 +8,7 @@ const Cover = ({setDisplay}) => {
     return (
         <div className='fixed w-full h-full flex justify-center items-center bg-gray-700 bg-opacity-40 z-20'>
             <div className='bg-yellow-300 w-48 h-auto p-6 flex justify-center flex-col rounded border border-black'>
-                <h1 className='text-center'>聲明</h1>
+                <h1 className='text-center text-xl'>聲明</h1>
                 <p>本平台所顯示之資訊僅供參考，實際狀況仍須自行詢問各社區之管理員或房東。</p>
                 <button onClick={clickHandler} className='shadow transform hover:scale-110 duration-300 mt-2 p-2 ring-2 ring-indigo-300 rounded bg-white w-auto h-auto '>確認</button>
             </div>
--- src/components/RentDetail.js
+++ src/components/RentDetail.js
@@ -5,8 +5,8 @@ const RentDetail = ({info}) => {
 
     return (
         <div
-            className='shadow shadow-lg border-2 border-gray-500 hover:border-gray-800 duration-300 bg-yellow-200 hover:bg-yellow-300 w-48 h-auto rounded m-auto mt-4 p-4 top-0 '>
-            <h1 className='text-center'>{info.name} {info.size}坪</h1>
+            className='shadow shadow-lg border-2 border-gray-500 hover:border-gray-800 duration-300 bg-yellow-200 w-48 h-auto rounded m-auto mt-4 p-4 top-0 '>
+            <h1 className='text-center text-lg'>{info.name} {info.size}坪</h1>
             <h1>房型：{info.roomType}</h1>
             <h1>{info.prize}元/月</h1>
             <h1>電費：{info.electric}</h1>
